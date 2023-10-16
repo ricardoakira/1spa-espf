bec090b5e6c42140561031b991a5f87dec69ac12
ricardoakira <rm551447@fiap.com.br>
2023-10-16 08:56:53 -0300

    segunda forma de preenchimento do form destrutuing

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -7,7 +7,12 @@ export default function EditarProdutos() {
 
     document.title = "Editar Produtos " + id; 
 
-    const[produto,setProduto] = useState({});
+    const[produto,setProduto] = useState({
+      id:id,
+      nome:'',
+      desc:'',
+      preco:''
+    });
 
     //Criar uma estratégia para recuperar o produto da API-JSON com fetch, utilizando GET:
     useEffect(()=>{
@@ -19,6 +24,21 @@ export default function EditarProdutos() {
 
     },[id]);
     
+    const handleChange = (e)=>{
+
+      //Destructuring
+      const {name,value} = e.target;
+
+      if(name == "nome"){
+        setProduto({"nome":value,"desc":produto.desc,"preco":produto.preco});
+      }else if(name == "desc"){
+        setProduto({"nome":produto.nome,"desc":value,"preco":produto.preco});
+      }else if(name == "preco"){
+        setProduto({"nome":"","desc":"","preco":value});
+      }
+      
+    }
+
 
   return (
     <div>
@@ -29,15 +49,15 @@ export default function EditarProdutos() {
                 <legend>Produto Selecionado</legend>
                 <div>
                   <label htmlFor="">Nome:</label>
-                  <input type="text" name="nome" placeholder="Digite o nome do Produto." value={produto.nome} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="nome" placeholder="Digite o nome do Produto." value={produto.nome} onChange={handleChange}/>
                 </div>
                 <div>
                   <label htmlFor="">Descrição:</label>
-                  <input type="text" name="desc" placeholder="Digite a descrição do Produto." value={produto.desc} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="desc" placeholder="Digite a descrição do Produto." value={produto.desc} onChange={handleChange}/>
                 </div>
                 <div>
                   <label htmlFor="">Preço:</label>
-                  <input type="text" name="preco" placeholder="Digite o preço do Produto." value={produto.preco} onChange={(e)=> setProduto(e.target.value)}/>
+                  <input type="text" name="preco" placeholder="Digite o preço do Produto." value={produto.preco} onChange={handleChange}/>
                 </div>
                 <div>
                   <button>EDITAR</button>
